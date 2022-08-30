e2a74e70698503a9d15c60549aaaa30d7d74b2e1
Nicolas Hedger <nicolas.hedger@e-nno.ch>
2022-08-30 15:19:22 +0200

    chore: update demo story

--- src/components/LinearStepper.story.vue
+++ src/components/LinearStepper.story.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-    <Story title="Linear Stepper" :layout="{type: 'single', iframe: false}">
+    <Story title="Linear Stepper" :layout="{ type: 'single', iframe: false }">
         <LinearStepper v-slot="{ current, previous, hasPrevious, next, hasNext }">
             <div>current step: {{ current }}</div>
             <div>has previous steps: {{ hasPrevious }}</div>
@@ -13,11 +13,20 @@
                 <button type="button" @click="previous" :disabled="!hasPrevious">Previous</button>
                 <button type="button" @click="next" :disabled="!hasNext">Next</button>
             </div>
-            <Step :position="0">
+            <Step :position="0" v-slot="{ isFirst, isLast }">
                 Step 0
+                <div>is first: {{ isFirst }}</div>
+                <div>is last: {{ isLast }}</div>
             </Step>
-            <Step :position="1">
+            <Step :position="1" v-slot="{ isFirst, isLast }">
                 Step 1
+                <div>is first: {{ isFirst }}</div>
+                <div>is last: {{ isLast }}</div>
+            </Step>
+            <Step :position="2" v-slot="{ isFirst, isLast }">
+                Step 2
+                <div>is first: {{ isFirst }}</div>
+                <div>is last: {{ isLast }}</div>
             </Step>
         </LinearStepper>
     </Story>
