a773ba3ee56212f140e30b4bf0ccbfb69c2ac7ca
Nicolas Hedger <nicolas.hedger@e-nno.ch>
2022-08-30 15:19:09 +0200

    feat: add `isFirst` and `isLast` slot props

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { inject, onMounted, Ref, ref } from 'vue';
+    import { computed, inject, onMounted, Ref, ref } from 'vue';
     import { registerInjectionKey } from '../inject';
 
     const props = defineProps<{
@@ -15,6 +15,33 @@
      * Current step of the parent stepper
      */
     const current = inject<Ref<number>>('current');
+
+    /**
+     * Reference to all the steps currently in the stepper
+     */
+    const steps = inject<Ref<Ref[]>>('steps');
+
+    /**
+     * Tells whether the step is the first step in the stepper
+     */
+    const isFirst = computed<boolean | undefined>(() => {
+        if (current?.value !== undefined && steps?.value !== undefined) {
+            return props.position === 0;
+        }
+
+        return undefined;
+    });
+
+    /**
+     * Tells whether the step is the last step in the stepper
+     */
+    const isLast = computed<boolean | undefined>(() => {
+        if (current?.value !== undefined && steps?.value !== undefined) {
+            return props.position === steps.value.length - 1;
+        }
+
+        return undefined;
+    });
 
     /**
      * A reference to the step component
@@ -35,5 +62,5 @@
 </script>
 
 <template>
-    <slot v-if="current === props.position" ref="step" />
+    <slot v-if="current === props.position" ref="step" :position="props.position" :is-first="isFirst" :is-last="isLast" />
 </template>
